7b93b2848de0f0ff58d4658540facc43993adb61
beacabdan <beatrizc.daniel@gmail.com>
2020-03-11 15:52:01 +0100

    Primera (y segunda?) clase del curso: introducción a condicionales, bucles, variables, etc.

---
+++ class1.py
@@ -0,0 +1,24 @@
+# PRINTS
+print("Hello Bea!")
+print("Hello Mundo!")
+print("-------------------")
+
+# CONDICIONALES
+edad = 28
+if edad >= 18:
+    print("Puedes entrar en...")
+else:
+    print("No puedes entrar en...")
+print("-------------------")
+
+# WHILE LOOP
+edad = 8
+while edad < 18:
+    print("No puedes entrar aquí!")
+    edad = edad + 1
+print("Bienvenido!")
+print("-------------------")
+
+# FOR LOOPS
+for x in range(0, 11):
+    print("Hola mundo!", x)
